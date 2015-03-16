560ea283bf29f3b29981a68c7cd26e58805da187
Redchards <urien.loic.cours@gmail.com>
2015-03-16 23:23:18 +0100

    deleted:    bin/debug/MemoryPool 	deleted:    bin/release/MemoryPool 	deleted:    build/Makefile 	deleted:    obj/debug/tst.d 	deleted:    obj/debug/tst.o 	deleted:    obj/release/tst.d 	deleted:    obj/release/tst.o

--- obj/debug/tst.d
+++
@@ -1,8 +0,0 @@
-../obj/debug/tst.o: ../src/tst.cxx ../include/MemoryPool.hxx \
-  ../include/Allocator.hxx ../include/CommonTypes.hxx
-
-../include/MemoryPool.hxx:
-
-../include/Allocator.hxx:
-
-../include/CommonTypes.hxx:
--- obj/release/tst.d
+++
@@ -1,8 +0,0 @@
-../obj/release/tst.o: ../src/tst.cxx ../include/MemoryPool.hxx \
-  ../include/Allocator.hxx ../include/CommonTypes.hxx
-
-../include/MemoryPool.hxx:
-
-../include/Allocator.hxx:
-
-../include/CommonTypes.hxx:
